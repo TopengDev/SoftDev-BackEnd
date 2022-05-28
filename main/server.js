2c1362fc98e91c7a97603ce786234111fbefa71c
TopengDev <topengdev@outlook.com>
2022-05-28 20:45:10 +0700

    commit

--- main/server.js
+++ main/server.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const path = require("path");
 const cors = require("cors");
 
@@ -17,7 +17,7 @@ app.use(cors());
 //   res.header("Access-Control-Allow-Origin", "*");
 // });
 
-app.use(express.static("./createTask"));
+// app.use(express.static("./createTask"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,5 +27,9 @@ app.use("/api/groups", require("./routes/groupRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
 app.use(errorHandler);
+
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("../client/build"));
+// }
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
